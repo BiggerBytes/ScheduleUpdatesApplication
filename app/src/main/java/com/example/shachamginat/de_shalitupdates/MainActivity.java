e5bb25be76fcaee586634def5a6f708c0e7c0718
Oren <kjhgfdsa100@gmail.com>
2015-10-01 17:57:49 +0300

    Changes to look and feel
    Changed the colors
    Added RTL support and forced it
    Added gradle libraries

--- app/src/main/java/com/example/shachamginat/de_shalitupdates/MainActivity.java
+++ app/src/main/java/com/example/shachamginat/de_shalitupdates/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.shachamginat.de_shalitupdates;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // force rtl layout
+        forceRtlIfSupported();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void forceRtlIfSupported() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        }
     }
 }
