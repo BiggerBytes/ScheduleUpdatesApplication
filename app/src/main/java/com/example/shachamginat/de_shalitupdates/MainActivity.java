271add92cae0f664e3b4b89329f7f67c543178c4
Oren <kjhgfdsa100@gmail.com>
2015-10-01 18:48:36 +0300

    Made things work
    Added recycler view and it's adapters
    Added local dummies to see the app in action
    Changed the style of the cards a little bit

--- app/src/main/java/com/example/shachamginat/de_shalitupdates/MainActivity.java
+++ app/src/main/java/com/example/shachamginat/de_shalitupdates/MainActivity.java
@@ -4,10 +4,20 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.View;
 
+import com.example.shachamginat.de_shalitupdates.serverdatarecievers.ScheduleChange;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<ScheduleChange> changes;
+    private ScheduleChangeAdapter adapter;
+    private RecyclerView rvChanges;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
         // force rtl layout
         forceRtlIfSupported();
+
+        // init recycler view
+        rvChanges = (RecyclerView) findViewById(R.id.rvChanges);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        rvChanges.setLayoutManager(llm);
+
+        // init changes list
+        changes = new ArrayList<>();
+        changes.add(new ScheduleChange(27, 2, "הילה כרמי", ScheduleChange.ChangeType.CANCELLED));
+        changes.add(new ScheduleChange(27, 4, "לאו קירשנבאום", ScheduleChange.ChangeType.CANCELLED));
+        changes.add(new ScheduleChange(26, 2, "וולאדי הגבר", ScheduleChange.ChangeType.CANCELLED));
+
+        adapter = new ScheduleChangeAdapter(MainActivity.this, changes);
+        rvChanges.setAdapter(adapter);
     }
 
     @Override
