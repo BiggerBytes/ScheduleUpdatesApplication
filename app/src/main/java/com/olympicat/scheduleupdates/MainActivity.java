77654a8e4118f64c6633731d34f47ef0912b9e64
Oren <kjhgfdsa100@gmail.com>
2015-10-09 18:12:57 +0300

    Removed spaces around dash when merging hours

--- app/src/main/java/com/olympicat/scheduleupdates/MainActivity.java
+++ app/src/main/java/com/olympicat/scheduleupdates/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
             String hour = "";
             for (int j = i + 1; j < this.changes.size(); ++j) {
                 if (changes.get(i).getTeacherName().equals(this.changes.get(j).getTeacherName())) {
-                    hour = this.changes.get(i).getHour() + " - " + this.changes.get(j).getHour();
+                    hour = this.changes.get(i).getHour() + "-" + this.changes.get(j).getHour();
                     Log.v(TAG, "Added index " + j + " to list");
                     removeList.add(this.changes.get(j));
                 } else {
