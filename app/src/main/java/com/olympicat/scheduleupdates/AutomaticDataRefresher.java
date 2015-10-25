ec180cb6569895ef260be78694a42fa84f924807
Avishay <avikillu@gmail.com>
2015-10-25 21:19:30 +0200

    Computer wins.
    (Re-) Added new notification icon.

--- app/src/main/java/com/olympicat/scheduleupdates/AutomaticDataRefresher.java
+++ app/src/main/java/com/olympicat/scheduleupdates/AutomaticDataRefresher.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.provider.Settings;
 import android.support.v4.app.NotificationManagerCompat;
@@ -29,7 +30,7 @@ import java.util.ArrayList;
 public class AutomaticDataRefresher extends IntentService {
 
     private static final String TAG = "AutomaticDataRefresher";
-    private static final long DELAY_TIME = 1000l * 60l * 5l; // 20 mins refresh interval
+    private static final long DELAY_TIME = 1000l * 60l * 20l; // 20 mins refresh interval
 
     private FileDataManager manager;
     private static SharedPreferences sp;
@@ -42,7 +43,8 @@ public class AutomaticDataRefresher extends IntentService {
 
     public void notifyUserOverScheduleChanges() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setSmallIcon(R.drawable.ic_schedule_white_48dp)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.happy))
                 .setContentTitle("שינויי מערכת")
                 .setContentText("יש עדכוני מערכת חדשים")
                 .setAutoCancel(true)
